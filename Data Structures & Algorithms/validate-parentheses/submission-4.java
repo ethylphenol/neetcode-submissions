26b319b8a41f31cd084b6c74d296737da779b1c0
ethylphenol <109046798+ethylphenol@users.noreply.github.com>
2026-07-14 20:58:15 +0200

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty())
+                    return false;
+                char opening = stack.peek();
+                if (opening == '(' && c == ')'
+                || opening == '[' && c == ']' ||
+                opening == '{' && c=='}') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+
+            }
+        }
+        return stack.isEmpty();
+    }
+}
