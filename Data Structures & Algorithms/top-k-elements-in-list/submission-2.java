b484c3acc8f9c6d997a657ae67be5de5a6d77026
ethylphenol <109046798+ethylphenol@users.noreply.github.com>
2026-07-17 02:36:42 +0200

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            counter.merge(num, 1, Integer::sum);
+        }
+        return counter.entrySet().stream().sorted(new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return -o1.getValue()+o2.getValue(); // padajuci
+            }
+        }).limit(k).mapToInt(e->e.getKey()).toArray();
+    }
+}
